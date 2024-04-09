93d483188e9ba2d65c8d69f8dbae2a2fd8ee7d1f
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-10 00:36:16 +0800

    initial commit with landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<img
+	src="images/landing_desktop_bg.png"
+	alt="desktopBG"
+	class="fixed bottom-0 left-0 right-0 top-0 -z-10 hidden h-full w-full lg:block"
+/>
+
+<div class="xs:px-[40px] xs:py-[88px] px-[55px] py-[32px]">
+	<a href="/admin/login" class=" text-[14px] font-semibold text-red-900 underline">ADMIN LOG IN</a>
+
+	<div
+		class="mt-[90px] flex flex-col lg:mx-auto lg:w-[714px] lg:rounded-[10px] lg:bg-[#d9d9d9de] lg:px-[68px] lg:py-[18px]"
+	>
+		<h1 class="xs:text-[28px] text-[24px] font-semibold leading-[30px] text-red-900 lg:text-center">
+			Web-Based Voting System for Lazaro Francisco Integrated School (SSG)
+		</h1>
+
+		<p class="mx-auto mt-[40px] text-[14px] leading-[28px] lg:w-[500px] lg:text-center">
+			Our system is designed to streamline the voting process for students, faculty, and staff of
+			Lazaro Francisco Integrated School, allowing for efficient and transparent elections for the
+			Student Supreme Government (SSG). With an intuitive interface and robust features, our
+			platform simplifies the creation of admin accounts, the management of voter accounts, and the
+			facilitation of the voting process.
+		</p>
+
+		<Button
+			class="xs:mt-[40px] mx-auto mt-[20px] w-[210px] rounded-[10px] bg-red-900 text-[14px] font-semibold"
+			>Click to Vote</Button
+		>
+	</div>
+</div>
