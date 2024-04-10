e64dcccfa36f8d5d39d8f8c30f6476a21056a080
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-11 02:39:05 +0800

    update all voters UI

---
+++ src/lib/route-components/admin/all-voters/import-data.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<label class="">
+	<div
+		class="flex h-[40px] cursor-pointer items-center rounded-lg bg-mainred px-[10px] text-[14px] font-semibold text-white transition-all hover:bg-clicked"
+	>
+		<p>Import Data</p>
+		<input type="file" class="hidden" />
+	</div>
+</label>
---
+++ src/routes/admin/(authenticated)/all-voters/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import * as Tabs from '$lib/components/ui/tabs';
+	import AllVoterFilters from '$lib/route-components/admin/all-voters/all-voter-filters.svelte';
+	import CreateVoter from '$lib/route-components/admin/all-voters/create-voter.svelte';
+	import ImportData from '$lib/route-components/admin/all-voters/import-data.svelte';
+</script>
+
+<div class="mt-[30px] p-[22px]">
+	<Tabs.Root value="highschool" class="w-full">
+		<div class="flex w-full flex-wrap justify-between gap-[10px]">
+			<div class="">
+				<Tabs.List class="">
+					<Tabs.Trigger class="w-full" value="highschool">High School</Tabs.Trigger>
+					<Tabs.Trigger class="w-full" value="elementary">Elementary</Tabs.Trigger>
+				</Tabs.List>
+			</div>
+
+			<div class="flex items-center gap-[10px]">
+				<div class="">
+					<CreateVoter />
+				</div>
+
+				<div class="">
+					<ImportData />
+				</div>
+			</div>
+		</div>
+
+		<div class="mt-[10px]">
+			<AllVoterFilters />
+		</div>
+
+		<Tabs.Content value="highschool" class="mt-[30px] ">highschool</Tabs.Content>
+		<Tabs.Content value="elementary" class="mt-[30px]">Elementary Votes</Tabs.Content>
+	</Tabs.Root>
+</div>
