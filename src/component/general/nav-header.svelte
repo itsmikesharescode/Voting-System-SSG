72f34e24d7d51cbc878fd76c1b76c136852b3f1e
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-10 14:35:52 +0800

    add logout for mobile

--- src/component/general/nav-header.svelte
+++ src/component/general/nav-header.svelte
@@ -2,6 +2,7 @@
 	import mobile_close_icon from '$lib/assets/mobile_close_icon.svg';
 	import mobile_open_icon from '$lib/assets/mobile_open_icon.svg';
 	import { selections } from '$lib';
+	import Button from '$lib/components/ui/button/button.svelte';
 
 	let showMobileMenu = false;
 </script>
@@ -10,7 +11,7 @@
 	class="bg-subwhite border-b-red flex h-[59px] w-full items-center justify-end border-b-[1px] px-[15px]"
 >
 	<button
-		class="rounded-[10px] p-[10px] transition-all active:scale-105"
+		class="rounded-[10px] p-[10px] transition-all active:scale-105 sm:hidden"
 		on:click={() => (showMobileMenu = !showMobileMenu)}
 	>
 		<img
@@ -22,9 +23,16 @@
 </nav>
 
 {#if showMobileMenu}
-	<menu class="bg-subwhite flex flex-col gap-[30px] border-b-[] py-[50px] text-[24px]">
+	<menu
+		class="bg-subwhite border-b-red relative flex flex-col gap-[30px] border-b-[1px] border-b-[] px-[75px] py-[50px] text-[24px] sm:hidden"
+	>
 		{#each selections as selection}
-			<a href={selection.url} class="text-red px-[75px]">{selection.title}</a>
+			<a href={selection.url} class="text-red max-w-fit">{selection.title}</a>
 		{/each}
+
+		<div class=" mt-[20px] flex items-center justify-end gap-[10px]">
+			<p class="text-red text-[14px]">Admin</p>
+			<Button>Log out</Button>
+		</div>
 	</menu>
 {/if}
