be715f05668efa24cba428ad46605e2c9c9f238e
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-10 23:59:06 +0800

    add initial admin state

---
+++ src/routes/admin/(authenticated)/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { setAdminState } from '$lib/stores';
+	import type { AdminState } from '$lib/types';
+
+	const initialState: AdminState = {
+		activeItem: '/admin/dashboard',
+		dashboard: null,
+		votes: null,
+		allvoters: null,
+		positions: null,
+		candidates: null
+	};
+
+	setAdminState(initialState);
+</script>
+
+<div class=""></div>
