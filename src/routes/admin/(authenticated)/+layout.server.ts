a15014b2ca65a5aea52207f1530c48afc0d8173a
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-11 15:38:06 +0800

    add admin content server routing

--- src/routes/admin/(authenticated)/+layout.server.ts
+++ src/routes/admin/(authenticated)/+layout.server.ts
@@ -1,5 +1,13 @@
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async () => {
+export const load: LayoutServerLoad = async ({ locals: { safeGetSession } }) => {
+    const { user } = await safeGetSession();
 
+    if (user) {
+        const { role } = user;
+        if (role !== "service_role") return redirect(301, "/voter/voting-process");
+    }
+
+    return redirect(301, "/?no-session");
 };
