528d4cf6e404b7084ac5f235556783c878056dd3
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-11 18:25:17 +0800

    add create voter schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -4,4 +4,21 @@ import { z } from "zod";
 export const adminLoginSchema = z.object({
     email: z.string().email(),
     password: z.string().min(1, { message: "Must enter a password." })
-})
+});
+
+export const createVoterAccount = z.object({
+    classification: z.string().min(1, { message: "Must choose a voter classification." }),
+    fullName: z.string().min(1, { message: "Must enter a valid full name." }),
+    voterLrn: z.string().min(11, { message: "Must enter a valid voter lrn." }),
+    email: z.string().email(),
+    password: z.string().min(6, { message: "Must choose a strong password." }),
+    confirmPassword: z.string()
+}).superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+        ctx.addIssue({
+            code: "custom",
+            message: "Password and Confirm Password must match",
+            path: ["confirmPassword"]
+        });
+    }
+});
