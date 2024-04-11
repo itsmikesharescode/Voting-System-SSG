e84b8d7dd44f68d277787ff694a3675b10e8c4cc
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-11 18:20:35 +0800

    add create voter server actions

--- src/routes/admin/(authenticated)/all-voters/+page.server.ts
+++ src/routes/admin/(authenticated)/all-voters/+page.server.ts
@@ -1,5 +1,17 @@
-import type { Actions } from "@sveltejs/kit";
+import { fail, type Actions } from "@sveltejs/kit";
+import type { ZodError } from "zod";
 
 export const actions: Actions = {
+    createVoterAction: async ({ locals: { supabase }, request }) => {
+        const formData = Object.fromEntries(await request.formData());
 
+        try {
+
+
+        } catch (error) {
+            const zodError = error as ZodError;
+            const { fieldErrors } = zodError.flatten();
+            return fail(400, { errors: fieldErrors });
+        }
+    }
 };
