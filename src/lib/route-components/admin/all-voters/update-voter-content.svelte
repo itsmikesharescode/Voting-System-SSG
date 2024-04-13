6dbbbc27c27c89f38c81e22effa9f625c57a7ae8
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-14 02:26:28 +0800

    add update voter UI

---
+++ src/lib/route-components/admin/all-voters/update-voter-content.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<Button class="w-full transition-all hover:bg-mainred">Update</Button>
--- src/lib/route-components/admin/all-voters/voters-table-card.svelte
+++ src/lib/route-components/admin/all-voters/voters-table-card.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
 	import { formatDate } from '$lib/helpers';
 	import { getAdminState, getUserState } from '$lib/stores';
+	import UpdateVoterContent from './update-voter-content.svelte';
 
 	const adminState = getAdminState();
 </script>
 
 <div class=" mt-[10px] text-[14px] font-semibold text-white xs:text-[16px] md:text-[18px]">
 	<div class="flex w-full justify-between border-b-[1px] border-b-subwhite bg-mainred p-[10px]">
-		<div class=" grid w-full grid-cols-4 gap-[20px] overflow-auto truncate">
+		<div class=" grid w-full grid-cols-[1fr,1fr,1fr,1fr,190px] gap-[20px] overflow-auto truncate">
 			<div class="grid overflow-auto">
 				<p class="">Voter Full Name</p>
 			</div>
@@ -21,19 +23,21 @@
 			<div class="grid overflow-auto">
 				<p class="">Date Created</p>
 			</div>
+
+			<div class="grid overflow-auto">
+				<p class="">Options</p>
+			</div>
 		</div>
 	</div>
 
 	<div class="max-h-[800px] w-full overflow-auto truncate bg-subwhite">
 		{#each $adminState.votes.userList ?? [] as voterObj}
 			<div
-				class=" grid w-full grid-cols-4 gap-[20px] border-b-[1px] border-mainred px-[10px] py-[5px] text-mainred"
+				class=" grid w-full grid-cols-[1fr,1fr,1fr,1fr,190px] gap-[20px] border-b-[1px] border-mainred px-[10px] py-[5px] text-mainred"
 			>
-				<button
-					class="grid max-w-fit overflow-auto transition-all duration-200 after:absolute after:right-full after:top-0 after:z-[-1] after:h-full after:w-full after:bg-white after:duration-200 hover:text-slate-950 hover:underline hover:after:translate-x-full"
-				>
-					{voterObj.user_fullname}
-				</button>
+				<div class="grid overflow-auto">
+					<p>{voterObj.user_fullname}</p>
+				</div>
 
 				<div class="grid overflow-auto">
 					<p>{voterObj.user_lrn}</p>
@@ -44,6 +48,13 @@
 
 				<div class="grid overflow-auto">
 					<p>{formatDate(voterObj.created_at)}</p>
+				</div>
+
+				<div class="w-full">
+					<div class="flex w-full items-center gap-[10px]">
+						<UpdateVoterContent />
+						<Button class="w-full transition-all hover:bg-mainred">Delete</Button>
+					</div>
 				</div>
 			</div>
 		{/each}
