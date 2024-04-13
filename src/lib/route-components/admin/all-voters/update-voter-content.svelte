ab81ab034ed8ddf0bcb1ffb4f91ab5938f4c0adc
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-14 02:37:32 +0800

    update voter UI

--- src/lib/route-components/admin/all-voters/update-voter-content.svelte
+++ src/lib/route-components/admin/all-voters/update-voter-content.svelte
@@ -1,5 +1,83 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import Label from '$lib/components/ui/label/label.svelte';
+
+	let showUpdateVoterDialoag = false;
 </script>
 
-<Button class="w-full transition-all hover:bg-mainred">Update</Button>
+<Button
+	class="w-full transition-all hover:bg-mainred"
+	on:click={() => (showUpdateVoterDialoag = true)}>Update</Button
+>
+
+<AlertDialog.Root bind:open={showUpdateVoterDialoag}>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>Mike John Eviota ?</AlertDialog.Title>
+			<AlertDialog.Description>This will update voters information.</AlertDialog.Description>
+
+			<div class=" flex flex-col gap-[20px] pt-[20px]">
+				<div class="grid w-full gap-1.5">
+					<Label class="text-left " for="fullName">Update Voter Full Name</Label>
+					<Input
+						name="fullName"
+						class="text-[14px] "
+						type="text"
+						id="fullName"
+						placeholder="Enter voter fullname"
+					/>
+				</div>
+
+				<div class="grid w-full gap-1.5">
+					<Label class="text-left " for="lrn">Update Voter LRN</Label>
+					<Input
+						name="lrn"
+						class="text-[14px] "
+						type="text"
+						id="lrn"
+						placeholder="Enter voter lrn"
+					/>
+				</div>
+
+				<div class="grid w-full gap-1.5">
+					<Label class="text-left " for="email">Update Voter Email</Label>
+					<Input
+						name="email"
+						class="text-[14px] "
+						type="email"
+						id="email"
+						placeholder="Enter voter email"
+					/>
+				</div>
+
+				<div class="grid w-full gap-1.5">
+					<Label class="text-left " for="password">Update Voter Password</Label>
+					<Input
+						name="password"
+						class="text-[14px] "
+						type="password"
+						id="password"
+						placeholder="Enter voter password"
+					/>
+				</div>
+
+				<div class="grid w-full gap-1.5">
+					<Label class="text-left " for="confirmPassword">Voter Confirm Password</Label>
+					<Input
+						name="confirmPassword"
+						class="text-[14px] "
+						type="password"
+						id="confirmPassword"
+						placeholder="Confirm voter password"
+					/>
+				</div>
+			</div>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+			<AlertDialog.Action>Continue</AlertDialog.Action>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
