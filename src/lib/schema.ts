4b061193de400d88acc98ae8da0e09b90759ce7e
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-15 17:21:19 +0800

    update schemas

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -31,20 +31,20 @@ export const migrationDataSchema = z.object({
 });
 
 export const updateVoterAccountSchema = z.object({
-    newClassification: z.string().refine((value) => value !== "undefined", {
+    classification: z.string().refine((value) => value !== "undefined", {
         message: "Must choose a voter classification."
     }),
-    newFullName: z.string().min(1, { message: "Must enter a valid full name." }),
-    newVoterLrn: z.string().min(11, { message: "Must enter a valid voter lrn." }),
-    newEmail: z.string().email(),
-    newPassword: z.string().min(6, { message: "Must choose a strong password." }),
-    confirmNewPassword: z.string()
-}).superRefine(({ confirmNewPassword, newPassword }, ctx) => {
-    if (newPassword !== confirmNewPassword) {
+    fullName: z.string().min(1, { message: "Must enter a valid full name." }),
+    voterLrn: z.string().min(11, { message: "Must enter a valid voter lrn." }),
+    email: z.string().email(),
+    password: z.string().min(6, { message: "Must choose a strong password." }),
+    confirmPassword: z.string()
+}).superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
         ctx.addIssue({
             code: "custom",
             message: "Password and Confirm Password must match",
-            path: ["confirmNewPassword"]
+            path: ["confirmPassword"]
         });
     }
 });
