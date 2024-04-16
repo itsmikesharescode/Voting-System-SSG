8588ad96f1b5b4705a1ff28e9780e7d70cfca0d1
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-16 16:51:47 +0800

    add mobile create form at admin all-voters route

--- src/lib/route-components/admin/all-voters/create-voter.svelte
+++ src/lib/route-components/admin/all-voters/create-voter.svelte
@@ -9,6 +9,7 @@
 	import type { ResultModel } from '$lib/types';
 	import { toast } from 'svelte-sonner';
 	import { invalidateAll } from '$app/navigation';
+	import * as Drawer from '$lib/components/ui/drawer';
 
 	const classifications = [
 		{ value: 'highschool', label: 'High School' },
@@ -59,30 +60,169 @@
 			await update();
 		};
 	};
+
+	let nativeWidthValue = 0;
 </script>
+
+<svelte:window bind:innerWidth={nativeWidthValue} />
 
 <Button
 	class="rounded-[10px] bg-mainred text-[14px] font-semibold"
 	on:click={() => (showCreateVoter = true)}>Create Voter</Button
 >
 
-<AlertDialog.Root bind:open={showCreateVoter}>
-	<AlertDialog.Content>
-		<form
-			method="post"
-			action="APIS?/createVoterAction"
-			enctype="multipart/form-data"
-			use:enhance={createVoterActionNews}
-		>
-			<AlertDialog.Header>
-				<AlertDialog.Title>Create Voter</AlertDialog.Title>
-				<AlertDialog.Description>
-					This will create a voter account for Lazaro Francisco Integrated School (SSG).
-				</AlertDialog.Description>
+{#if nativeWidthValue > 1024}
+	<!--Forms for Desktop-->
+	<AlertDialog.Root bind:open={showCreateVoter}>
+		<AlertDialog.Content>
+			<form
+				method="post"
+				action="APIS?/createVoterAction"
+				enctype="multipart/form-data"
+				use:enhance={createVoterActionNews}
+			>
+				<AlertDialog.Header>
+					<AlertDialog.Title>Create Voter</AlertDialog.Title>
+					<AlertDialog.Description>
+						This will create a voter account for Lazaro Francisco Integrated School (SSG).
+					</AlertDialog.Description>
 
-				<div class=" flex flex-col gap-[20px] pt-[20px]">
+					<div class=" flex flex-col gap-[20px] pt-[20px]">
+						<Select.Root>
+							<Select.Trigger class="w-full " disabled={createVoterLoader}>
+								<Select.Value placeholder="Choose voter classification" />
+							</Select.Trigger>
+							<Select.Content class="mt-[10px]">
+								<Select.Group>
+									<Select.Label class="text-left">Select Voter Classification</Select.Label>
+									{#each classifications as classification}
+										<Select.Item value={classification.value} label={classification.label}
+											>{classification.label}</Select.Item
+										>
+									{/each}
+								</Select.Group>
+							</Select.Content>
+							<Select.Input name="classification" />
+						</Select.Root>
+						{#each formActionErrors?.classification ?? [] as errorMsg}
+							<p class="text-left text-[14px] text-red-600">{errorMsg}</p>
+						{/each}
+
+						<div class="grid w-full gap-1.5">
+							<Label class="text-left " for="fullName">Voter Full Name</Label>
+							<Input
+								disabled={createVoterLoader}
+								name="fullName"
+								class="text-[14px] "
+								type="text"
+								id="fullName"
+								placeholder="Enter voter fullname"
+							/>
+							{#each formActionErrors?.fullName ?? [] as errorMsg}
+								<p class="text-left text-[14px] text-red-600">{errorMsg}</p>
+							{/each}
+						</div>
+
+						<div class="grid w-full gap-1.5">
+							<Label class="text-left  " for="voterLrn">Voter LRN</Label>
+							<Input
+								disabled={createVoterLoader}
+								name="voterLrn"
+								class="text-[14px] "
+								type="text"
+								id="voterLrn"
+								placeholder="Enter voter lrn"
+							/>
+							{#each formActionErrors?.voterLrn ?? [] as errorMsg}
+								<p class="text-left text-[14px] text-red-600">{errorMsg}</p>
+							{/each}
+						</div>
+
+						<div class="grid w-full gap-1.5">
+							<Label class="text-left  " for="email">Voter Email</Label>
+							<Input
+								disabled={createVoterLoader}
+								name="email"
+								class="text-[14px] "
+								type="email"
+								id="email"
+								placeholder="Enter voter email"
+							/>
+							{#each formActionErrors?.email ?? [] as errorMsg}
+								<p class="text-left text-[14px] text-red-600">{errorMsg}</p>
+							{/each}
+						</div>
+
+						<div class="grid w-full items-center gap-1.5">
+							<Label class="text-left  " for="password">Voter Password</Label>
+							<Input
+								disabled={createVoterLoader}
+								name="password"
+								class="text-[14px] "
+								type="password"
+								id="password"
+								placeholder="Enter voter password"
+							/>
+							{#each formActionErrors?.password ?? [] as errorMsg}
+								<p class="text-left text-[14px] text-red-600">{errorMsg}</p>
+							{/each}
+						</div>
+
+						<div class="grid w-full items-center gap-1.5">
+							<Label class="text-left  " for="confirmPassword">Voter Confirm Password</Label>
+							<Input
+								disabled={createVoterLoader}
+								name="confirmPassword"
+								class="text-[14px] "
+								type="password"
+								id="confirmPassword"
+								placeholder="Enter voter password"
+							/>
+							{#each formActionErrors?.confirmPassword ?? [] as errorMsg}
+								<p class="text-left text-[14px] text-red-600">{errorMsg}</p>
+							{/each}
+						</div>
+					</div>
+				</AlertDialog.Header>
+				<AlertDialog.Footer class="mt-[20px]">
+					<AlertDialog.Cancel on:click={() => (formActionErrors = null)}>Cancel</AlertDialog.Cancel>
+					<Button
+						disabled={createVoterLoader}
+						type="submit"
+						class="{createVoterLoader ? 'cursor-not-allowed bg-clicked' : 'bg-mainred'}
+					 text-[14px] font-semibold"
+						on:click={() => (showCreateVoter = true)}
+					>
+						{#if createVoterLoader}
+							Creating...
+						{:else}
+							Create Voter
+						{/if}
+					</Button>
+				</AlertDialog.Footer>
+			</form>
+		</AlertDialog.Content>
+	</AlertDialog.Root>
+{:else}
+	<!--Forms for Mobile Screens-->
+	<Drawer.Root bind:open={showCreateVoter}>
+		<Drawer.Content>
+			<form
+				method="post"
+				action="APIS?/createVoterAction"
+				enctype="multipart/form-data"
+				use:enhance={createVoterActionNews}
+			>
+				<Drawer.Header>
+					<Drawer.Title>Create Voter</Drawer.Title>
+					<Drawer.Description
+						>This will create a voter account for Lazaro Francisco Integrated School (SSG).</Drawer.Description
+					>
+				</Drawer.Header>
+
+				<div class=" flex h-[400px] flex-col gap-[20px] overflow-auto p-[20px]">
 					<Select.Root>
-						<Select.Trigger class="w-full" disabled={createVoterLoader}>
+						<Select.Trigger class="w-full " disabled={createVoterLoader}>
 							<Select.Value placeholder="Choose voter classification" />
 						</Select.Trigger>
 						<Select.Content class="mt-[10px]">
@@ -176,23 +316,24 @@
 						{/each}
 					</div>
 				</div>
-			</AlertDialog.Header>
-			<AlertDialog.Footer class="mt-[20px]">
-				<AlertDialog.Cancel on:click={() => (formActionErrors = null)}>Cancel</AlertDialog.Cancel>
-				<Button
-					disabled={createVoterLoader}
-					type="submit"
-					class="{createVoterLoader ? 'cursor-not-allowed bg-clicked' : 'bg-mainred'}
+
+				<Drawer.Footer>
+					<Button
+						disabled={createVoterLoader}
+						type="submit"
+						class="{createVoterLoader ? 'cursor-not-allowed bg-clicked' : 'bg-mainred'}
 					 text-[14px] font-semibold"
-					on:click={() => (showCreateVoter = true)}
-				>
-					{#if createVoterLoader}
-						Creating...
-					{:else}
-						Create Voter
-					{/if}
-				</Button>
-			</AlertDialog.Footer>
-		</form>
-	</AlertDialog.Content>
-</AlertDialog.Root>
+						on:click={() => (showCreateVoter = true)}
+					>
+						{#if createVoterLoader}
+							Creating...
+						{:else}
+							Create Voter
+						{/if}
+					</Button>
+					<Drawer.Close>Cancel</Drawer.Close>
+				</Drawer.Footer>
+			</form>
+		</Drawer.Content>
+	</Drawer.Root>
+{/if}
