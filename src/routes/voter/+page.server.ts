27076745562bb4a4228b845687cd0abe42e123ca
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-18 14:30:17 +0800

    update voter login server actions*

--- src/routes/voter/+page.server.ts
+++ src/routes/voter/+page.server.ts
@@ -1,30 +1,44 @@
 import { fail, type Actions } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import type { PostgrestError } from "@supabase/supabase-js";
+import type { ZodError } from "zod";
+import { voterLoginSchema } from "$lib/schema";
 
 export const load: PageServerLoad = async ({ locals: { supabaseAdmin } }) => {
 
 };
 
 export const actions: Actions = {
-    loginAction: async ({ locals: { supabase, supabaseAdmin } }) => {
-        const { data: checkRecord, error: checkRecordError } = await supabaseAdmin.rpc("login_checker", {
-            email_param: "andrewbayudan@gmail.com",
-            password_param: "default password",
-        }) as { data: boolean, error: PostgrestError | null };
+    loginAction: async ({ locals: { supabase, supabaseAdmin }, request }) => {
 
-        if (checkRecordError) return fail(401, { msg: checkRecordError.message });
+        const formData = Object.fromEntries(await request.formData());
 
-        else if (checkRecord) return fail(201, { msg: "Not Registered." });
+        try {
+            const result = voterLoginSchema.parse(formData);
 
-        const { data: { user }, error: loginError } = await supabase.auth.signInWithPassword({
-            email: "",
-            password: "",
-        });
+            const { data: checkRecord, error: checkRecordError } = await supabaseAdmin.rpc("login_checker", {
+                email_param: "andrewbayudan@gmail.com",
+                password_param: "default password",
+            }) as { data: boolean, error: PostgrestError | null };
 
-        if (loginError) return fail(401, { msg: loginError.message });
+            if (checkRecordError) return fail(401, { msg: checkRecordError.message });
 
-        else if (user) return fail(200, { msg: "Login Success." });
+            else if (checkRecord) return fail(201, { msg: "Not Registered." });
+
+            const { data: { user }, error: loginError } = await supabase.auth.signInWithPassword({
+                email: "",
+                password: "",
+            });
+
+            if (loginError) return fail(401, { msg: loginError.message });
+
+            else if (user) return fail(200, { msg: "Login Success." });
+        } catch (error) {
+            const zodError = error as ZodError;
+            const { fieldErrors } = zodError.flatten();
+            return fail(400, { errors: fieldErrors });
+        }
+
 
 
     }
