49abe4f1fb1e8094afc5e1d7f1f74531b9f3ae86
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-20 23:57:31 +0800

    update reset pws server actions

--- src/routes/voter/forgot-password/update-password/+page.server.ts
+++ src/routes/voter/forgot-password/update-password/+page.server.ts
@@ -1,6 +1,7 @@
 import type { ZodError } from "zod";
 import type { Actions, PageServerLoad } from "./$types";
 import { fail } from "@sveltejs/kit";
+import { updateAccountSchema, updatePasswordSchema } from "$lib/schema";
 
 export const load: PageServerLoad = async ({ locals: { supabase } }) => {
 
@@ -11,6 +12,24 @@ export const actions: Actions = {
         const formData = Object.fromEntries(await request.formData());
 
         try {
+            const result = updatePasswordSchema.parse(formData);
+
+            const { data: { user }, error: updatePwsErr } = await supabase.auth.updateUser({
+                password: result.password
+            });
+
+            if (updatePwsErr) return fail(401, { msg: updatePwsErr.message });
+            else if (user) {
+                const { error: updateUserListErr } = await supabase.from("user_list_tb").update([{
+                    user_password: result.password
+                }]).eq("user_id", user.id);
+
+                if (updateUserListErr) return fail(401, { msg: updateUserListErr.message });
+                else return fail(200, { msg: "Password Updated Successfully." });
+            };
+
+
+
 
         } catch (error) {
             const zodError = error as ZodError;
