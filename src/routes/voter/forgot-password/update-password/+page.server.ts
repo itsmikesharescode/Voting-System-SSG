c2847f552e542d6eb6298b724a4985d2e51ca5ec
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-20 23:43:00 +0800

    update server actions for update pws

--- src/routes/voter/forgot-password/update-password/+page.server.ts
+++ src/routes/voter/forgot-password/update-password/+page.server.ts
@@ -1,11 +1,21 @@
+import type { ZodError } from "zod";
 import type { Actions, PageServerLoad } from "./$types";
+import { fail } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ locals: { supabase } }) => {
 
 };
 
 export const actions: Actions = {
-    updatePassword: async ({ locals: { supabase } }) => {
+    updatePassword: async ({ locals: { supabase }, request }) => {
+        const formData = Object.fromEntries(await request.formData());
 
+        try {
+
+        } catch (error) {
+            const zodError = error as ZodError;
+            const { fieldErrors } = zodError.flatten();
+            return fail(400, { errors: fieldErrors });
+        }
     }
 };
