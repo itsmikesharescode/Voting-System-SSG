a9cb6c2fcef6c41cb5b59a92f163848a74825504
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-20 19:35:59 +0800

    add data fetching for voted candidates

--- src/routes/voter/(authenticated)/+layout.server.ts
+++ src/routes/voter/(authenticated)/+layout.server.ts
@@ -16,7 +16,8 @@ export const load: LayoutServerLoad = async ({ locals: { supabase, supabaseAdmin
 
             return {
                 userData: await supabaseAdmin.from("user_list_tb").select("*").eq("user_id", user?.id).single() as PostgrestSingleResponse<UserListDB>,
-                candidates: await supabaseAdmin.from("created_candidates_tb").select("*").eq("classification", user.user_metadata.classification).order('created_at', { ascending: true }) as PostgrestSingleResponse<CandidatesDB[]>
+                candidates: await supabaseAdmin.from("created_candidates_tb").select("*").eq("classification", user.user_metadata.classification).order('created_at', { ascending: true }) as PostgrestSingleResponse<CandidatesDB[]>,
+                votedCandidates: await supabaseAdmin.from("vote_records_tb").select("*").eq("user_id", user.id).order("created_at", { ascending: true })
             }
 
         } else return redirect(301, "/voter");
