6274ada1bf3d11e6513c640d4222d887e5e61d94
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-20 20:18:03 +0800

    update stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,6 +2,7 @@ import { writable } from "svelte/store";
 import type { Writable } from "svelte/store";
 import { getContext, setContext } from "svelte";
 import type { AdminState, UserListDB } from "./types";
+import type { SupabaseClient } from "@supabase/supabase-js";
 
 export const setAdminState = (state: AdminState) => {
     const stateGenerator = writable(state);
@@ -16,4 +17,11 @@ export const setUserState = (state: UserListDB | null) => {
 };
 
 export const getUserState = () => getContext<Writable<UserListDB | null>>("userState");
+
+export const setSupaBaby = (state: SupabaseClient) => {
+    const stateGenerator = writable(state);
+    setContext("supababy", stateGenerator);
+};
+
+export const getSupaBaby = () => getContext<SupabaseClient>("supababy");
 
