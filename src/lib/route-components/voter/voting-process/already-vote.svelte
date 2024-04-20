237b68eebc317e94bdc53222d68669ebf16bebb1
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-20 19:33:30 +0800

    update view ballot

--- src/lib/route-components/voter/voting-process/already-vote.svelte
+++ src/lib/route-components/voter/voting-process/already-vote.svelte
@@ -1,21 +1,42 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
+	import ViewBallot from './already-voted/view-ballot.svelte';
+	import ViewRealtimeResult from './already-voted/view-realtime-result.svelte';
+
+	let showBallot = false;
+	let showRealTimeResult = false;
 </script>
 
-<Card.Root class="mx-auto mt-[100px] max-w-[800px]">
-	<Card.Header>
-		<Card.Title
-			class="text-center text-[16px] font-semibold text-mainred xs:text-[18px] sm:text-[20px] md:text-[22px] lg:text-[24px]"
-			>YOU HAVE ALREADY VOTED FOR THIS ELECTION</Card.Title
-		>
-		<Card.Description class="text-center"
-			>You can view your recent votes by clicking view ballot. You can also see the latest votes by
-			clicking the View Realtime Result</Card.Description
-		>
-	</Card.Header>
-	<Card.Content class="m-[40px] flex flex-col gap-[20px]">
-		<Button>VIEW YOUR BALLOT</Button>
-		<Button>VIEW REAL TIME RESULT</Button>
-	</Card.Content>
-</Card.Root>
+{#if showBallot}
+	<ViewBallot bind:showBallot />
+{:else if showRealTimeResult}
+	<ViewRealtimeResult />
+{:else}
+	<Card.Root class="mx-auto mt-[100px] max-w-[800px]">
+		<Card.Header>
+			<Card.Title
+				class="text-center text-[16px] font-semibold text-mainred xs:text-[18px] sm:text-[20px] md:text-[22px] lg:text-[24px]"
+				>YOU HAVE ALREADY VOTED FOR THIS ELECTION</Card.Title
+			>
+			<Card.Description class="text-center"
+				>You can view your recent votes by clicking view ballot. You can also see the latest votes
+				by clicking the View Realtime Result</Card.Description
+			>
+		</Card.Header>
+		<Card.Content class="m-[40px] flex flex-col gap-[20px]">
+			<Button
+				on:click={() => {
+					showBallot = true;
+					showRealTimeResult = false;
+				}}>VIEW YOUR BALLOT</Button
+			>
+			<Button
+				on:change={() => {
+					showBallot = false;
+					showRealTimeResult = true;
+				}}>VIEW REAL TIME RESULT</Button
+			>
+		</Card.Content>
+	</Card.Root>
+{/if}
---
+++ src/lib/route-components/voter/voting-process/already-voted/view-ballot.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let showBallot = false;
+</script>
+
+<div class="">
+	This is ballot
+
+	<button on:click={() => (showBallot = false)}>asdasd</button>
+</div>
