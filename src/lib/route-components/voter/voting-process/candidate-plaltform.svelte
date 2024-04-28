b0de3f55314e3849c3a92f778cc407e5e3465b97
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-28 13:52:57 +0800

    add candidate photo when viewing platform

--- src/lib/route-components/voter/voting-process/candidate-plaltform.svelte
+++ src/lib/route-components/voter/voting-process/candidate-plaltform.svelte
@@ -2,7 +2,7 @@
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index';
 	import { Button } from '$lib/components/ui/button/index';
 	import type { Candidates } from '$lib/types';
-
+	import * as Avatar from '$lib/components/ui/avatar';
 	export let candidate: Candidates;
 
 	let showPlatformDialog = false;
@@ -13,12 +13,17 @@
 >
 
 <AlertDialog.Root bind:open={showPlatformDialog}>
-	<AlertDialog.Content>
+	<AlertDialog.Content class=" max-w-[700px]">
 		<AlertDialog.Header>
+			<Avatar.Root class="h-[250px] w-full rounded-[20px]">
+				<Avatar.Image src={candidate.candidatePhoto} alt="@shadcn" />
+				<Avatar.Fallback>CN</Avatar.Fallback>
+			</Avatar.Root>
+
 			<AlertDialog.Title>{candidate.candidateName}'s Platform</AlertDialog.Title>
-			<AlertDialog.Description class="max-h-[500px] overflow-auto"
-				>{candidate.candidateMotto}</AlertDialog.Description
-			>
+			<AlertDialog.Description class="max-h-[250px] overflow-auto "
+				>{candidate.candidateMotto}
+			</AlertDialog.Description>
 		</AlertDialog.Header>
 		<AlertDialog.Footer>
 			<AlertDialog.Action>Back</AlertDialog.Action>
